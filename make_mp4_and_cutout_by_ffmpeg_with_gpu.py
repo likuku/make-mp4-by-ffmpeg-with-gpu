36434185d1ae3205116db9ce1f60151f1ca931ba
likuku-macbook-pro <likuku.public@gmail.com>
2017-11-09 16:40:09 +0800

    fanc/python/make_mp4_and_cutout_by_ffmpeg_with_gpu.py update make_cmd_array_for_bmd_recorder() make_cmd_array_for_other() for opts

--- make_mp4_and_cutout_by_ffmpeg_with_gpu.py
+++ make_mp4_and_cutout_by_ffmpeg_with_gpu.py
@@ -1,7 +1,7 @@
 '''
 Copy Right by likuku
 kuku.li@fanc.co
-last update on Nov1,2017
+last update on Nov9,2017
 先决条件:
 安装 ffmpeg-static for windows,给当前用户增加环境变量
 安装 python3 for windows,默认安装 # .py 会与 python 解析器自动关联
@@ -15,7 +15,7 @@ import datetime
 print('请关闭系统里其他占用GPU的程序：3D游戏,3D渲染工具,AdobePR,AdobeMediaEncoder 等\n')
 print('请输入素材文件路径 :' )
 _src_video_name_input=input().replace('"','')
-# 路径里包含空格，则拖拽文件时，windows 会自动给首尾加一对双引号，subprocess 不需要 
+# 路径里包含空格，则拖拽文件时，windows 会自动给首尾加一对双引号，subprocess 不需要
 _src_video_file_name = _src_video_name_input.rsplit('\\',1)[1]
 _src_video_path = _src_video_name_input.rsplit('\\',1)[0]
 _check_file_name = lambda _x : (_x.rsplit('.',1)[0].count('Capture') +
@@ -76,8 +76,7 @@ def make_cmd_array_for_bmd_recorder():
         '-map','0:2',
         '-b:v','%s' % _bitrate,
         '-t','%s' % _duration,
-        '-pix_fmt','yuv420p',
-        '%s.mp4' % _dst_video_file]
+        '-pix_fmt','yuv420p']
     return(_cmd_array)
 
 def make_cmd_array_for_other():
@@ -87,15 +86,14 @@ def make_cmd_array_for_other():
         '-c:v','%s' % _src_codec_video_input,
         '-b:v','%s' % _bitrate,
         '-t','%s' % _duration,
-        '-pix_fmt','yuv420p',
-        '%s.mp4' % _dst_video_file]
+        '-pix_fmt','yuv420p']
     return(_cmd_array)
 
 if _src_video_made_by_bmd:
     pass
-    _cmd = make_cmd_array_for_bmd_recorder()
+    _cmd = make_cmd_array_for_bmd_recorder() + ['%s.mp4' % _dst_video_file]
 else:
-    _cmd = make_cmd_array_for_other()
+    _cmd = make_cmd_array_for_other() + ['%s.mp4' % _dst_video_file]
 
 print(_cmd)
 subprocess.call(_cmd)
