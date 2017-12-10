715984e4d040b5797f9aed5b7bcc563340dc448c
likuku-new-macbook <likuku.public@gmail.com>
2017-12-11 01:01:56 +0800

    fanc/python test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py clear more pass

--- test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py
+++ test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py
@@ -3,15 +3,14 @@ from make_mp4_and_cutout_by_ffmpeg_with_gpu import *
 
 class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
     def test_check_str_raw_src_media_path(self):
-        pass
         self.assertEqual(False,
                          check_str_raw_src_media_path(''))
         self.assertEqual(False,
                          check_str_raw_src_media_path('.13123241'))
         self.assertEqual(True,
                          check_str_raw_src_media_path('.'))
+
     def test_rebuild_list_str_src_media_path(self):
-        pass
         self.assertEqual(['/path/subPath','file.ext'],
              rebuild_list_str_src_media_path('/path/subPath/file.ext',path_split_by))
         self.assertEqual(['/path/subPath','file.ext'],
@@ -20,16 +19,16 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
              rebuild_list_str_src_media_path('/path/subPath/file.ext ',path_split_by))
         self.assertEqual(['/path/sub\ Path','file.ext'],
              rebuild_list_str_src_media_path('/path/sub\ Path/file.ext ',path_split_by))
+
     def test_rebuild_bool_deinterlace(self):
-        pass
         self.assertTrue(rebuild_bool_deinterlace('1'))
         self.assertFalse(rebuild_bool_deinterlace('0'))
         self.assertFalse(rebuild_bool_deinterlace(''))
         #with self.assertRaises(TypeError):
         #    value = rebuild_bool_deinterlace('balbala')
         #self.assertEqual(False,rebuild_bool_deinterlace('balabla'))
+
     def test_check_and_rebuild_str_codec_video(self):
-        pass
         self.assertEqual('h264_nvenc',
                          check_and_rebuild_str_codec_video('0'))
         self.assertEqual('libx264',
@@ -42,14 +41,14 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                          check_and_rebuild_str_codec_video('4'))
         self.assertEqual('copy',
                          check_and_rebuild_str_codec_video('5'))
+
     def test_check_and_rebuild_str_bitrate_video(self):
-        pass
         self.assertEqual('100M',
                          check_and_rebuild_str_bitrate_video(''))
         self.assertEqual('10M',
                          check_and_rebuild_str_bitrate_video('10'))
+
     def test_set_bitrate_for_3Dtop2Dleft_from_list_for_cmd_array(self):
-        pass
         self.assertEqual([['crop=iw:(ih/2):0:0,scale=iw:(ih*2)',
                            '-b:v','40M']],
              set_bitrate_for_3Dtop2Dleft_from_list_for_cmd_array(
@@ -59,6 +58,7 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
              set_bitrate_for_3Dtop2Dleft_from_list_for_cmd_array(
                  [['yadif=1,crop=iw:(ih/2):0:0,scale=iw:(ih*2)','-b:v','100M']],
                  '40M'))
+
     def test_rebuild_str_timestamp_input(self):
         self.assertEqual('01:01:01',
                          rebuild_str_timestamp_input('01:01:01'))
@@ -68,16 +68,19 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                          rebuild_str_timestamp_input('100:1:1'))
         self.assertEqual('01:01:00',
                          rebuild_str_timestamp_input('01:01:0'))
+
     def test_make_str_duration(self):
         self.assertEqual('999999999',make_str_duration('00:00:00',None))
         self.assertEqual('60',make_str_duration('00:00:00','00:01:00'))
         self.assertEqual('60',make_str_duration('01:01:53','01:02:53'))
+
     def test_make_str_dst_video_file(self):
         self.assertEqual('/path%sfile.ext_00_00_00_to_00_00_60' % path_split_by,
                          make_str_dst_video_file('00:00:00',
                                                  '00:00:60',
                                                  '/path',
                                                  'file.ext'))
+
     def test_make_array_vf(self):
         self.assertEqual(['-vf', 'yadif=1,crop=iw:(ih/2):0:0,scale=iw:(ih*2)'],
                          make_array_vf(True,True))
@@ -87,6 +90,7 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                          make_array_vf(True,False))
         self.assertEqual(['-vf', 'crop=iw:(ih/2):0:0,scale=iw:(ih*2)'],
                          make_array_vf(False,True))
+
     def test_make_cmd_array_for_copy(self):
         self.assertEqual(['%s' % ffmpeg_name,
                           '-ss','00:00:00',
@@ -97,6 +101,7 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                                                  '/path/file.ext',
                                                  'copy',
                                                  '60'))
+
     def test_make_cmd_array_for_other(self):
         self.assertEqual(['%s' % ffmpeg_name,
                           '-ss','00:00:00',
@@ -113,6 +118,7 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                                                  'libx264',
                                                  '100M',
                                                  '60'))
+
     def test_make_list_for_cmd_array(self):
         _dict_input = {'_src_video_name_input':'/path/file.ext',
                        '_bool_video_deinterlace':False,
@@ -125,6 +131,7 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                        '_str_bitrate_3dt2dl':None,
                        '_bool_double_action_3dt2dl':False,
                        '_bool_rewrite_output':False}
+        pass
         self.assertEqual([['ffmpeg',
                            '-ss','00:00:00',
                            '-i','/path/file.ext',
