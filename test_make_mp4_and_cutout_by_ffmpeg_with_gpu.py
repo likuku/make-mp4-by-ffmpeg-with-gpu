c47cab2502c3666d09825d54ac3a602541daa991
likuku-new-macbook <likuku.public@gmail.com>
2017-12-14 00:00:27 +0800

    fanc/python/test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py add test for [h264_nvenc,deinterlace,100M,60sec,3tb2d,40M,DoubleAction,noOverWriter] into test_make_list_for_cmd_array() and it passed the unit test

--- test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py
+++ test_make_mp4_and_cutout_by_ffmpeg_with_gpu.py
@@ -272,6 +272,52 @@ class Test_make_mp4_and_cutout_by_ffmpeg_with_gpu(unittest.TestCase):
                  _dict_input['_bool_double_action_3dt2dl'],
                  _dict_input['_bool_rewrite_output']))
         pass
+        # h264_nvenc,deinterlace,100M,60sec,3tb2d,40M,DoubleAction,noOverWriter
+        _dict_input['_str_codec_video'] = 'h264_nvenc'
+        _dict_input['_str_bitrate'] = '100M'
+        _dict_input['_bool_video_deinterlace'] = True
+        _dict_input['_bool_make_2d_l_from_top'] = True
+        _dict_input['_str_bitrate_3dt2dl'] = '40M'
+        _dict_input['_bool_double_action_3dt2dl'] = True
+        _dict_input['_bool_rewrite_output'] = False
+        self.assertEqual([['ffmpeg',
+                           '-ss','00:00:00',
+                           '-i','/path/file.ext',
+                           '-c:v','h264_nvenc',
+                           '-map','0:v',
+                           '-ac','2',
+                           '-map','0:a',
+                           '-b:v','100M',
+                           '-t','60',
+                           '-pix_fmt','yuv420p',
+                           '-aspect','16:9',
+                           '-vf','yadif=1',
+                           '/path/file.ext_00_00_00_to_00_01_00.mp4'],
+                          ['ffmpeg',
+                           '-ss','00:00:00',
+                           '-i','/path/file.ext',
+                           '-c:v','h264_nvenc',
+                           '-map','0:v',
+                           '-ac','2',
+                           '-map','0:a',
+                           '-b:v','40M',
+                           '-t','60',
+                           '-pix_fmt','yuv420p',
+                           '-aspect','16:9',
+                           '-vf','yadif=1,crop=iw:(ih/2):0:0,scale=iw:(ih*2)',
+                           '/path/file.ext_00_00_00_to_00_01_00_3DTop2Left_2D.mp4']],
+             make_list_for_cmd_array(_dict_input['_src_video_name_input'],
+                 _dict_input['_bool_video_deinterlace'],
+                 _dict_input['_str_codec_video'],
+                 _dict_input['_str_bitrate'],
+                 _dict_input['_start_timestamp'],
+                 _dict_input['_duration'],
+                 _dict_input['_dst_video_file'],
+                 _dict_input['_bool_make_2d_l_from_top'],
+                 _dict_input['_str_bitrate_3dt2dl'],
+                 _dict_input['_bool_double_action_3dt2dl'],
+                 _dict_input['_bool_rewrite_output']))
+        pass
 
 
 if __name__ == '__main__':
